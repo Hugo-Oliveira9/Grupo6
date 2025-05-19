cd1adbb6819a1f230cc6cd68ce054d40fc44ca66
Guilherme <guilherme.silva700@fatec.sp.gov.br>
2025-05-19 08:51:15 -0300

    criação-e-adição-dos-valores-dos-produtos-no-carrinho

--- JavaScript/loja.js
+++ JavaScript/loja.js
@@ -2,5 +2,5 @@ function adicionar(produto) {
   let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
   carrinho.push(produto);
   localStorage.setItem('carrinho', JSON.stringify(carrinho));
-  alert(`"${produto.nome}" foi adicionado ao carrinho!`);
+  alert(`${produto.nome} R$ ${produto.valor} reais foi adicionado ao carrinho!`);
 }
